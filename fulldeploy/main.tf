30e472585d34577ba0a3246fc89dc02271fc05be
Michael <michael.digiorgio70@qmail.cuny.edu>
2022-06-15 16:35:17 -0400

    env

--- fulldeploy/main.tf
+++ fulldeploy/main.tf
@@ -41,6 +41,7 @@ locals {
 
 module "networking" {
   source = "./terraform-aws-networking"
+  vpc_name = "New"
 
   public_private_subnet_pairs = local.subnets
   vpc_primary_cidr            = local.vpc_cidr // will be determined by dev/staging/prod vars
