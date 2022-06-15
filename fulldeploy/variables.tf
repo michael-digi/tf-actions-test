9b7c8e695a4211d837bb49365b12980848790228
Michael <michael.digiorgio70@qmail.cuny.edu>
2022-06-14 21:51:09 -0400

    alb

--- fulldeploy/variables.tf
+++ fulldeploy/variables.tf
@@ -26,3 +26,9 @@ variable "subnets" {
   type        = number
   default     = 3
 }
+
+variable "environment" {
+    description = "Environment deploying to"
+    type        = string
+    default     = "dev"
+}
