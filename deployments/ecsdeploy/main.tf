26ba81c39ea7d552d8ef4c1f27e256c4d345f6bd
Michael <michael.digiorgio70@qmail.cuny.edu>
2022-06-18 23:57:47 -0400

    local var typo

--- deployments/ecsdeploy/main.tf
+++ deployments/ecsdeploy/main.tf
@@ -68,7 +68,7 @@ locals {
     value = [for s in data.aws_subnet.public : s.cidr_block]
   }
 
-  private_subnet_cidr_blocks {
+  private_subnet_cidr_blocks = {
     value = [for s in data.aws_subnet.private : s.cidr_block]
   }
 }
