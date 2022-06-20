50da791b6037796bad9c6dd4ec73691859f50cf3
Michael <michael.digiorgio70@qmail.cuny.edu>
2022-06-19 21:36:10 -0400

    production

--- deployments/ecrdeploy/variables.tf
+++ deployments/ecrdeploy/variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "us-west-2"
 }
+
+variable "app_name" {
+  description = "Name of portal app"
+  type        = string
+  default     = "gck-portal"
+}
