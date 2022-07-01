5c47c2a6b4022837f69d8379d8ab4e168de7a8ec
Michael <michael.digiorgio70@qmail.cuny.edu>
2022-07-01 17:15:21 -0400

    build out of mongo cluster/services/tasks/nlb

--- terraform-aws-mongo/ecs_task.tf
+++ terraform-aws-mongo/ecs_task.tf
@@ -8,52 +8,12 @@ resource "aws_ecs_task_definition" "gck_mongo1" {
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([{
     name      = "monogo1-container-${var.env}"
-    image     = var.mongo_image
+    image     = "417363389520.dkr.ecr.us-east-1.amazonaws.com/mongocluster:latest"
     essential = true
     portMappings = [{
       protocol      = "tcp"
       containerPort = 27017
       hostPort      = 27017
-    }]
-  }])
-}
-
-resource "aws_ecs_task_definition" "gck_mongo2" {
-  network_mode             = "awsvpc"
-  family                   = "mongo"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
-  container_definitions = jsonencode([{
-    name      = "monogo2-container-${var.env}"
-    image     = var.mongo_image
-    essential = true
-    portMappings = [{
-      protocol      = "tcp"
-      containerPort = 27018
-      hostPort      = 27018
-    }]
-  }])
-}
-
-resource "aws_ecs_task_definition" "gck_mongo3" {
-  network_mode             = "awsvpc"
-  family                   = "mongo"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
-  container_definitions = jsonencode([{
-    name      = "monogo3-container-${var.env}"
-    image     = var.mongo_image
-    essential = true
-    portMappings = [{
-      protocol      = "tcp"
-      containerPort = 27019
-      hostPort      = 27019
     }]
   }])
 }
--- terraform-aws-mongo/variables.tf
+++ terraform-aws-mongo/variables.tf
@@ -1,3 +1,11 @@
 variable "mongo_image" {
   default = "image"
 }
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "env" {
+    default = "production"
+}
