72bb58196c5ef9d463608c16df0bb2ff1b6a2a22
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-07-26 22:04:16 -0400

    s3 backend

--- deployments/remote-backend-deploy/main.tf
+++ deployments/remote-backend-deploy/main.tf
@@ -10,9 +10,33 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_dynamodb_table" "terraform-state" {
+ name           = "terraform-state"
+ read_capacity  = 20
+ write_capacity = 20
+ hash_key       = "LockID"
+
+ attribute {
+   name = "LockID"
+   type = "S"
+ }
+}
+
+resource "aws_kms_key" "terraform-bucket-key" {
+ description             = "This key is used to encrypt bucket objects"
+ deletion_window_in_days = 10
+ enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "key-alias" {
+ name          = "alias/terraform-bucket-key"
+ target_key_id = aws_kms_key.terraform-bucket-key.key_id
+}
+
 
 module "remote_backend_s3" {
   source = "../../terraform-aws-backend-s3"
   env = var.env
+  kms_arn = aws_kms_key.terraform-bucket-key.arn
   region = var.region
 }
--- terraform-aws-backend-s3/s3.tf
+++ terraform-aws-backend-s3/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "terraform-state" {
  server_side_encryption_configuration {
    rule {
      apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
+       kms_master_key_id = var.kms_arn
        sse_algorithm     = "aws:kms"
      }
    }
