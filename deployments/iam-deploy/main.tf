c482ee6e7e2b5faaed97bdbb5e5cf3ec9dce606d
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-03 18:32:22 -0400

    mongo adjustment

---
+++ deployments/iam-deploy/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "iam" {
+  source = "../../modules/terraform-aws-iam"
+}
