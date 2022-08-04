1c78e5a4598829e303566761c511370e761a8191
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-04 19:43:41 -0400

    efs sg

--- modules/terraform-aws-mongo/efs.tf
+++ modules/terraform-aws-mongo/efs.tf
@@ -8,6 +8,6 @@ resource "aws_efs_mount_target" "mongo_replica" {
   file_system_id = aws_efs_file_system.mongo_replica.id
   subnet_id      = each.key
   security_groups = [
-    aws_security_group.vpc_access.id,
+    aws_security_group.efs.id,
   ]
 }
--- modules/terraform-aws-mongo/sg.tf
+++ modules/terraform-aws-mongo/sg.tf
@@ -3,14 +3,6 @@ resource "aws_security_group" "vpc_access" {
   vpc_id = var.vpc_id
 
   ingress {
-    protocol         = "ICMP"
-    from_port        = 0
-    to_port          = 0
-    cidr_blocks      = ["192.168.0.0/16"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
     protocol         = "-1"
     from_port        = 0
     to_port          = 0
@@ -25,4 +17,87 @@ resource "aws_security_group" "vpc_access" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+}
+
+resource "aws_security_group" "mongo" {
+  name   = "vpc-access-sg-${var.env}-${var.region}"
+  vpc_id = var.vpc_id
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 27017
+    to_port          = 0
+    cidr_blocks      = ["192.168.0.0/16"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "mongo" {
+  name   = "mongo-sg-${var.env}-${var.region}"
+  vpc_id = var.vpc_id
+  
+  description = "Access between Mongo nodes."
+  
+  tags = {
+    Name = "${var.env}_omnilert_private_vpc_access"
+  }
+}
+
+resource "aws_security_group_rule" "mongo_ingress" {
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 27017
+  to_port           = 27017
+  security_group_id = aws_security_group.mongo.id
+  source_security_group_id = aws_security_group.mongo.id
+}
+
+resource "aws_security_group_rule" "mongo_egress" {
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  security_group_id = aws_security_group.mongo.id
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "aws_security_group" "efs" {
+  name   = "efs-sg-${var.env}-${var.region}"
+  vpc_id = var.vpc_id
+  
+  description = "Access between Mongo nodes."
+  
+  tags = {
+    Name = "${var.env}_omnilert_private_vpc_access"
+  }
+}
+
+resource "aws_security_group_rule" "efs_ingress" {
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 2049
+  to_port           = 2049
+  security_group_id = aws_security_group.efs.id
+  source_security_group_id = aws_security_group.mongo.id
+}
+
+resource "aws_security_group_rule" "efs_egress" {
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  security_group_id = aws_security_group.efs.id
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
 }
