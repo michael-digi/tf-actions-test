4e0dec0905dfc26e2da2e5f0d74a64ff91fe877c
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-04 00:21:36 -0400

    efs sg

--- modules/terraform-aws-mongo/sg.tf
+++ modules/terraform-aws-mongo/sg.tf
@@ -48,3 +48,25 @@ resource "aws_security_group" "efs" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_security_group" "vpc_access" {
+  name   = "vpc-access-sg-${var.env}-${var.region}"
+  vpc_id = var.vpc_id
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["192.168.0.0/16"]
+    security_groups = [aws_security_group.mongo.id]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
