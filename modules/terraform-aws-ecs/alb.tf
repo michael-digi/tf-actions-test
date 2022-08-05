d2077a2f590938841d31adaabc859ff7c27ab9a8
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-05 10:46:22 -0400

    portal logs

--- modules/terraform-aws-ecs/alb.tf
+++ modules/terraform-aws-ecs/alb.tf
@@ -1,43 +1,43 @@
-resource "aws_lb" "gck_portal" {
-  name               = "${var.app_name}-alb-${var.env}"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups = [
-    aws_security_group.alb.id
-  ]
-  subnets = var.public_subnets
+# resource "aws_lb" "gck_portal" {
+#   name               = "${var.app_name}-alb-${var.env}"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups = [
+#     aws_security_group.alb.id
+#   ]
+#   subnets = var.public_subnets
 
-  enable_deletion_protection = false
-}
+#   enable_deletion_protection = false
+# }
 
-resource "aws_alb_target_group" "gck_portal" {
-  name        = "${var.app_name}-tg-${var.env}"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
+# resource "aws_alb_target_group" "gck_portal" {
+#   name        = "${var.app_name}-tg-${var.env}"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = var.vpc_id
+#   target_type = "ip"
 
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
-}
+#   health_check {
+#     healthy_threshold   = "3"
+#     interval            = "30"
+#     protocol            = "HTTP"
+#     matcher             = "200"
+#     timeout             = "3"
+#     path                = "/"
+#     unhealthy_threshold = "2"
+#   }
+# }
 
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = aws_lb.gck_portal.id
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_alb_listener" "http" {
+#   load_balancer_arn = aws_lb.gck_portal.id
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    target_group_arn = aws_alb_target_group.gck_portal.id
-    type             = "forward"
-  }
-}
+#   default_action {
+#     target_group_arn = aws_alb_target_group.gck_portal.id
+#     type             = "forward"
+#   }
+# }
 
 # resource "aws_alb_listener" "https" {
 #   load_balancer_arn = aws_lb.gck_portal.id
