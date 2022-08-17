071b0d77e7152655668402eb0762abc9ec0dce3a
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-16 20:19:51 -0400

    route53 test

--- modules/terraform-aws-mongo/route53.tf
+++ modules/terraform-aws-mongo/route53.tf
@@ -11,5 +11,5 @@ resource "aws_route53_record" "mongo_node_dns" {
   zone_id = aws_route53_zone.private.zone_id
   name    = "mongo0${count.index + 1}.miked.com"
   type    = "A"
-  ttl     = 0
+  ttl     = 1
 }
