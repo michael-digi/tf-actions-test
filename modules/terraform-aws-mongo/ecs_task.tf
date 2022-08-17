b8308ab9068ba7774c5fb6c553db809bba59bc75
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-16 20:50:30 -0400

    route53 test

--- modules/terraform-aws-mongo/ecs_task.tf
+++ modules/terraform-aws-mongo/ecs_task.tf
@@ -1,19 +1,19 @@
 resource "aws_ecs_task_definition" "gck_mongo1" {
   network_mode             = "awsvpc"
-  family                   = "mongo1"
+  family                   = "mongo01"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 1024
   memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   volume {
-    name = "mongo_volume_1"
+    name = "mongo_volume_01"
     efs_volume_configuration {
       file_system_id = aws_efs_file_system.mongo_replica.id
     }
   }
   container_definitions = jsonencode([{
-    name      = "mongo1-container-${var.env}"
+    name      = "mongo01-container-${var.env}"
     image     = "417363389520.dkr.ecr.us-east-1.amazonaws.com/mongocluster:latest"
     essential = true
     portMappings = [{
@@ -23,12 +23,12 @@ resource "aws_ecs_task_definition" "gck_mongo1" {
     }]
     mountPoints = [{
       containerPath : "/data"
-      sourceVolume : "mongo_volume_1"
+      sourceVolume : "mongo_volume_01"
     }]
     "environment" = [
       { "name" : "DOMAIN", "value" : "${aws_route53_zone.private.name}." },
-      { "name" : "SUB_DOMAIN", "value" : "${aws_route53_record.mongo_node_dns[0].name}." },
-      { "name" : "DATA_DIR", "value" : "mongo1" },
+      { "name" : "SUB_DOMAIN", "value" : "mongo01.${aws_route53_zone.private.name}." },
+      { "name" : "DATA_DIR", "value" : "mongo01" },
       { "name" : "LEADER", "value" : "true" }
     ]
     linuxParameters = {
@@ -48,7 +48,7 @@ resource "aws_ecs_task_definition" "gck_mongo1" {
 
 resource "aws_ecs_task_definition" "gck_mongo2" {
   network_mode             = "awsvpc"
-  family                   = "mongo2"
+  family                   = "mongo02"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 1024
   memory                   = 2048
@@ -61,7 +61,7 @@ resource "aws_ecs_task_definition" "gck_mongo2" {
     }
   }
   container_definitions = jsonencode([{
-    name      = "mongo2-container-${var.env}"
+    name      = "mongo02-container-${var.env}"
     image     = "417363389520.dkr.ecr.us-east-1.amazonaws.com/mongocluster:latest"
     essential = true
     portMappings = [{
@@ -71,12 +71,12 @@ resource "aws_ecs_task_definition" "gck_mongo2" {
     }]
     mountPoints = [{
       containerPath : "/data"
-      sourceVolume : "mongo_volume_2"
+      sourceVolume : "mongo_volume_02"
     }]
     "environment" = [
       { "name" : "DOMAIN", "value" : "${aws_route53_zone.private.name}." },
-      { "name" : "SUB_DOMAIN", "value" : "${aws_route53_record.mongo_node_dns[1].name}." },
-      { "name" : "DATA_DIR", "value" : "mongo2" },
+      { "name" : "SUB_DOMAIN", "value" : "mongo02.${aws_route53_zone.private.name}." },
+      { "name" : "DATA_DIR", "value" : "mongo02" },
       { "name" : "LEADER", "value" : "false" }
     ]
     linuxParameters = {
@@ -96,20 +96,20 @@ resource "aws_ecs_task_definition" "gck_mongo2" {
 
 resource "aws_ecs_task_definition" "gck_mongo3" {
   network_mode             = "awsvpc"
-  family                   = "mongo3"
+  family                   = "mongo03"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 1024
   memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   volume {
-    name = "mongo_volume_3"
+    name = "mongo_volume_03"
     efs_volume_configuration {
       file_system_id = aws_efs_file_system.mongo_replica.id
     }
   }
   container_definitions = jsonencode([{
-    name      = "mongo3-container-${var.env}"
+    name      = "mongo03-container-${var.env}"
     image     = "417363389520.dkr.ecr.us-east-1.amazonaws.com/mongocluster:latest"
     essential = true
     portMappings = [{
@@ -119,12 +119,12 @@ resource "aws_ecs_task_definition" "gck_mongo3" {
     }]
     mountPoints = [{
       containerPath : "/data"
-      sourceVolume : "mongo_volume_3"
+      sourceVolume : "mongo_volume_03"
     }]
     "environment" = [
       { "name" : "DOMAIN", "value" : "${aws_route53_zone.private.name}." },
-      { "name" : "SUB_DOMAIN", "value" : "${aws_route53_record.mongo_node_dns[2].name}." },
-      { "name" : "DATA_DIR", "value" : "mongo3" },
+      { "name" : "SUB_DOMAIN", "value" : "mongo03.${aws_route53_zone.private.name}." },
+      { "name" : "DATA_DIR", "value" : "mongo03" },
       { "name" : "LEADER", "value" : "false" }
     ],
     linuxParameters = {
--- modules/terraform-aws-mongo/route53.tf
+++ modules/terraform-aws-mongo/route53.tf
@@ -5,11 +5,3 @@ resource "aws_route53_zone" "private" {
     vpc_id = var.vpc_id
   }
 }
-
-resource "aws_route53_record" "mongo_node_dns" {
-  count = 3
-  zone_id = aws_route53_zone.private.zone_id
-  name    = "mongo0${count.index + 1}.miked.com"
-  type    = "A"
-  ttl     = 1
-}
