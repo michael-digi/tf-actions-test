6cce01ac47dc888db70b1c583e34edf5b793a85d
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-28 17:05:34 -0400

    pull test

--- deployments/network-deploy/dev/us-east-2/main.tf
+++ deployments/network-deploy/dev/us-east-2/main.tf
@@ -16,74 +16,28 @@ provider "aws" {
 
 locals {
   subnet_pairs = {
-    production = flatten([
-    {
-      az          = "${var.region}a"
-      cidr        = "172.16.64.0/20"
-      public_cidr = "172.16.0.0/20"
-    },
-    {
-      az          = "${var.region}b"
-      cidr        = "172.16.80.0/20"
-      public_cidr = "172.16.16.0/20"
-    },
-    {
-      az          = "${var.region}c"
-      cidr        = "172.16.96.0/20"
-      public_cidr = "172.16.32.0/20"
-    },
-    var.subnets == 4 ? [{
-      az          = "${var.region}d"
-      cidr        = "172.16.112.0/20"
-      public_cidr = "172.16.48.0/20"
-    }] : []
-  ])
-
-  staging = flatten([
-    {
-      az          = "${var.region}a"
-      cidr        = "192.168.64.0/20"
-      public_cidr = "192.168.0.0/20"
-    },
-    {
-      az          = "${var.region}b"
-      cidr        = "192.168.80.0/20"
-      public_cidr = "192.168.16.0/20"
-    },
-    {
-      az          = "${var.region}c"
-      cidr        = "192.168.96.0/20"
-      public_cidr = "192.168.32.0/20"
-    },
-    var.subnets == 4 ? [{
-      az          = "${var.region}d"
-      cidr        = "192.168.112.0/20"
-      public_cidr = "192.168.48.0/20"
-    }] : []
-  ])
-
-  dev = flatten([
-    {
-      az          = "${var.region}a"
-      cidr        = "10.0.64.0/20"
-      public_cidr = "10.0.0.0/20"
-    },
-    {
-      az          = "${var.region}b"
-      cidr        = "10.0.80.0/20"
-      public_cidr = "10.0.16.0/20"
-    },
-    {
-      az          = "${var.region}c"
-      cidr        = "10.0.96.0/20"
-      public_cidr = "10.0.32.0/20"
-    },
-    var.subnets == 4 ? [{
-      az          = "${var.region}d"
-      cidr        = "10.0.112.0/20"
-      public_cidr = "10.0.48.0/20"
-    }] : []
-  ])
+    dev = flatten([
+      {
+        az          = "${var.region}a"
+        cidr        = "10.0.64.0/20"
+        public_cidr = "10.0.0.0/20"
+      },
+      {
+        az          = "${var.region}b"
+        cidr        = "10.0.80.0/20"
+        public_cidr = "10.0.16.0/20"
+      },
+      {
+        az          = "${var.region}c"
+        cidr        = "10.0.96.0/20"
+        public_cidr = "10.0.32.0/20"
+      },
+      var.subnets == 4 ? [{
+        az          = "${var.region}d"
+        cidr        = "10.0.112.0/20"
+        public_cidr = "10.0.48.0/20"
+      }] : []
+    ])
   }
 }
 
