b1915d7d5c7ae8bf6733087db4befe57abadd881
Michael DiGiorgio <michael.digiorgio70@qmail.cuny.edu>
2022-08-31 21:00:23 -0400

    restructuring

--- modules/terraform-aws-mongo/ecs_service.tf
+++ modules/terraform-aws-mongo/ecs_service.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_service" "gck_mongo" {
   name                               = "mongo0${count.index+1}-service-${var.env}-${var.region}"
   cluster                            = aws_ecs_cluster.gck_mongo.id
   task_definition                    = aws_ecs_task_definition.gck_mongo[count.index].arn
-  desired_count                      = 0
+  desired_count                      = 1
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
   launch_type                        = "FARGATE"
